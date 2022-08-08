6f6ffdcc530645447b92912797133e5faa4e6050
antonraguzin <raguzin82@gmail.com>
2022-08-08 16:03:45 +0300

    added fallen test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -6,18 +6,27 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanTest {
     @Test
-    void postmanTest() {
-        // Given - When - Then
-// Предусловия
+    void postmanTestV1() {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
                 .when()
                 .post("/post")
-// Проверки
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("some data"));
     }
+    @Test
+    void postmanTestV2() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("data"));
+    }
 }
+
+
